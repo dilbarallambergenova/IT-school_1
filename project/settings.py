8c7601886d2336ce4b60d9999f87aa88ce2594d4
dilbarallambergenova <allambergenovadilbar9@gmail.com>
2025-08-21 16:31:10 +0500

    Add STATIC_ROOT for Heroku

--- project/settings.py
+++ project/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_URL=[str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT=str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
